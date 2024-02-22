deffc5a66a29904f7d3a1596167a072164501693
Abdulmalik-amh <mloqk21@gmail.com>
2024-02-22 23:17:19 +0300

    added script for enabling item
    commented script for enabling the item in the hierarchy and disabling the other items images implementation well be done Saturday.
    Changed images name to image 1, image 2.......
    Changed Buttons name
    Main image goes to "Panel"
    
    *** There is a bug when pressing the side Image on the left it disappear
    @kaita404

---
+++ MainProject/Assets/Abdulmalik/EnableItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnableItem : MonoBehaviour
+{
+
+    public GameObject [] Items;
+    public Image [] Images;
+
+    public void ItemEnable(){
+
+        // if item active in herarichy = false 
+        // other items images set active false
+        // edite canvas set active true 
+        // item set active true
+
+    }
+}
